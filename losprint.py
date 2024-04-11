67e3207d886e6fbf77ce3f085cfea7ea401a2b0e
Fernando Alvarenga <166652229+Eraf4@users.noreply.github.com>
2024-04-11 15:33:44 -0400

    Los print
    Ejemplo de print en python

---
+++ losprint.py
@@ -0,0 +1,9 @@
+
+nombre="fernando"
+password=1234
+
+print("Su nombre es:",nombre,"y su contraseña:",password)
+print("Su nombre es:"+nombre,"y su contraseña: "+str(password))
+print("Su nombre es:{} y su contraseña:{}".format(nombre,password))
+print("Su nombre es:%s y su contraseña:%d"%(nombre,password))
+print(f"Su nombre es:{nombre} y su contraseña:{password}")
