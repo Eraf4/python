0aced84e6aa9717c8799a7ff7aaafe6489f5dc07
Fernando Alvarenga <166652229+Eraf4@users.noreply.github.com>
2024-04-11 15:02:19 -0400

    Ecuación Cuadrática
    Ejercicio de ecuación cuadrática

---
+++ ecuacion_cuadratica.py
@@ -0,0 +1,9 @@
+#ecuacion cuadratica
+#Resolver la ecuación cuadrática: 2x**2-7x+3=0
+a = 2
+b = -7
+c = 3
+x1= (-b + (b**2 - 4*a*c)**(1/2) )/ (2*a)
+x2= (-b - (b**2 - 4*a*c)**(1/2) )/ (2*a)
+print("x1:",x1)
+print("x2:",x2)
